079a95157a4bc810e9668aca3257ae33b1f0bd40
Rayane <2111752@cegepmontpetit.ca>
2023-02-03 14:13:50 -0500

    Ajout des class

--- super_Musique_Infinie/src/app/app.component.ts
+++ super_Musique_Infinie/src/app/app.component.ts
@@ -7,8 +7,11 @@ import { lastValueFrom } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //initialisation des variables
   title = 'super_Musique_Infinie';
   artist : string = "";
+  Chansons : Chanson[] = [];
+  Albums : Album[] = [];
   similarArtist : string[] = [];//liste de string vide
   result = false;
   apiKey : string = "e34ebf8561ba7c653a21d1d99a1a0070"
@@ -17,32 +20,21 @@ export class AppComponent {
 
   }
   async searchArtist(): Promise<void>{
-    //oublie pas le route de l'api
-    let x = await lastValueFrom(this.http.get<any>("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+this.artist+"&api_key="+this.apiKey+"&format=json"))
-    //log in la console
-    console.log("------l'objet parent")
-    console.log(x);
-    console.log("------l'objet enfant array")
-    console.log(x.similarartists);
-    console.log("------premiere element de l'array")
-    console.log(x.similarartists.artist[0]);
-    console.log("------nom du premiere element de l'array")
-    console.log(x.similarartists.artist[0].name);
-    //push vers le tableau
-    console.log("liste de tout les artistes")
-    for(let artist of x.similarartists.artist){
-      console.log(artist.name);
-      this.similarArtist.push(artist.name);
-    }
-
     this.result = true;
-
-	// La requête HTTP devra être ajoutée ici
-
+  }
+  async searchAlbum(): Promise<void>{
   }
   newSearch():void{
     //vider la liste hehehehehehehe
     this.similarArtist.splice(0);
     this.result = false;
   }
+}
+//Classe Album
+class Album {
+  constructor(public titre: string, public artiste: string, public chansons: Chanson[]) {}
+}
+//Classe chanson
+class Chanson {
+  constructor(public titre: string, public duree: number) {}
 }
