dceacbdd5881cbbbfc3334a4995b2b3118c3d4bd
Rayane <2111752@cegepmontpetit.ca>
2023-02-09 21:15:29 -0500

    PROGRESS WOOHOO
    progres progres progres we now need to display une bonne ptit liste woohooo WOOHOO

--- super_Musique_Infinie/src/app/app.component.ts
+++ super_Musique_Infinie/src/app/app.component.ts
@@ -13,41 +13,73 @@ export class AppComponent   {
   Chansons : Chanson[] = [];
   Albums : Album[] = [];
   //similarArtist : string[] = [];//liste de string vide
-  result = false;
   apiKey : string = "9a8a3facebbccaf363bb9fd68fa37abf";
+  monterAlbum : boolean = false;
   //Constructeur (pour les dependance seulement)
   constructor(public http : HttpClient) {
-    let i : number = 0;
-    i++;
-  }
 
-  async searchArtist(): Promise<void>{
-    let nomArtiste = "2pac";
-    console.log("btn chercher artist pressed");
-    //let x = await lastValueFrom(this.http.get<any>("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="+nomArtiste+"r&api_key="+this.apiKey));
-    let x = await lastValueFrom(this.http.get<any>("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=2pac&api_key=9a8a3facebbccaf363bb9fd68fa37abf&format=json"));
-    let bol : boolean = false;
   }
+  //methode qui cherche les top albums
   async searchTopAlbum(nomArtist : String): Promise<void>{
-    let constructeurDeRequete = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist="+nomArtist+"&api_key="+this.apiKey+"&format=json";
-    let topAlbumsResponse = await lastValueFrom(this.http.get<any>(constructeurDeRequete));
+    let constructeurDeRequeteAlbum = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist="+nomArtist+"&api_key="+this.apiKey+"&format=json";
+    let topAlbumsResponse = await lastValueFrom(this.http.get<any>(constructeurDeRequeteAlbum));
     //initialize an empty array
-  
+    console.log(topAlbumsResponse);
+    for (const album of topAlbumsResponse.topalbums.album) {
+        //recuperer l'objet image qui a le tag extraLarge (meilleur resolution)
+        //creation d'un pseudo objet image qui a deux attribut : size et #text present dans la reponse JSON
+        const image = album.image.find((img: { size: string, "#text": string}) => img.size === 'extralarge');
+        //recuperer l'adress de l'image si elle existe
+        const imageUrl = image ? image["#text"] : '';
+        //creation de l'objet album
+        let albumCourant : Album = new Album(album.name, album.artist.name, album.playcount, imageUrl , []);
+        //remplir l'album de chanson
+        this.remplirChanson(albumCourant);
+        this.Albums.push(albumCourant);
+
+      }
+      this.monterAlbum = true;
+    ;
+    console.log(await lastValueFrom(this.http.get<any>("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=9a8a3facebbccaf363bb9fd68fa37abf&artist=Cher&album=Believe&format=json")));
+
   }
-  newSearch():void{
-    //vider la liste hehehehehehehe
-    
-    //this.similarArtist.splice(0);
-    this.result = false;
+  //method qui recupere le nom
+  viderAlbum():void{
+    this.Albums = [];
+    this.monterAlbum = false;
+  }
+  async remplirChanson(monAlbum : Album): Promise<void>{
+    let constructeurDeRequeteMusic = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key="+this.apiKey+"&artist="+monAlbum.artiste+"&album="+monAlbum.titre+"&format=json";
+    let reponse = await lastValueFrom(this.http.get<any>(constructeurDeRequeteMusic));
+    let chansons = reponse.album.tracks.track;
+    for (const objChansonCourante of chansons) {
+      if (objChansonCourante != undefined){
+        let chanson = new Chanson(objChansonCourante.name, objChansonCourante.duration);
+        monAlbum.chansons.push(chanson);
+        console.log("chansons "+ chanson.titre + " ajoute a l'album");
+      }
+
   }
 
-  
+
+
 
 
 }
+montrerChanson(displayedAlbum : Album){
+  for(const chanson  of displayedAlbum.chansons){
+    alert(chanson.titre);
+  }
+}
+}
+//////////CLASSES
 //Classe Album
 class Album {
-  constructor(public titre: string, public artiste: string, public nbrEcoute : number  ) {}
+  constructor(public titre: string,
+              public artiste: string,
+              public nbrEcoute : number ,
+              public imageUrl :string ,
+              public chansons : (Chanson[])) {}
 }
 //Classe chanson
 class Chanson {
